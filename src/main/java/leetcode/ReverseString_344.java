e67ad0c41e9d77bad9c880a225caeb515ae84406
Ankit Patel <ankit.patel.3794@gmail.com>
2022-09-04 07:00:46 +0530

    added reverse string

---
+++ src/main/java/leetcode/ReverseString_344.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ */
+public class ReverseString_344 {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while(start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
